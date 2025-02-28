f5341d91e29cd5f8e8f2b1b8b655b5e1193a3fa4
aliser <20380250+murolem@users.noreply.github.com>
2025-02-28 15:51:00 +1000

    Version 1.1
    - Updated defaults to have `100` quality.
    - Added a resize option based on megapixel limit (turned on by default). By default, it's set to 64 MP, which is the Cosmoteer wiki limit on image area size.
    - Added a resize option based on largest dimension (turned off by default). Not used, but might be useful for some cases ig.
    - Updated docs to include features. Added an example command showing all options.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-This Python script is used to convert PNGs and JPGs to WEBPs, while removing extra whitespace around each image.
+This Python script is used to convert ship images from Cosmoteer.
 
-This is specifically for processing exported ship images from Cosmoteer.
+Features:
+- Conversion from PNGs and JPGs to WEBPs
+- Removal of extra whitespace.
+- Resizing based on largest side.
+- Resizing based on amount of pixels (megapixels).
+
+This is specifically for processing exported ship images from Cosmoteer, but can be used for any other image as well.
 
 # Installation
 
@@ -22,12 +28,21 @@ Run:
 python main.py
 ```
 
-This will process all the images, converting them to webp with `90` quality and removing whitespace. Note that original images will be removed.
+By default, for each image:
+- Quality is set to `100`.
+- Extra whitespace is trimmed.
+- If image is larger than 64 MP, it will be shrunk until it's equal to or below the limit.
+- After processing, the original image **is removed**.
 
 If an image is already a webp image, it would be processed anyway.
 
-For convenience, both the quality and the whitespace removal options can be configured via arguments. For help, type:
+For further configuring, type:
 
 ```bash
 python main.py --help
+```
+
+All options:
+```bash
+python main.py -q <integer> -r <integer> -mp <integer> --keep-whitespace
 ```
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from math import floor, sqrt
 from os import listdir
 from PIL import Image, ImageChops
 import webp
@@ -8,16 +9,23 @@ from pathlib import Path
 arg_parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
-arg_parser.add_argument("-q", help="WEBP image quality, from 0 to 100", default=90, type=int)
+arg_parser.add_argument("-q", help="WEBP image quality, from 0 to 100", default=100, type=int)
+arg_parser.add_argument("-r", help="Upper bound for any image dimension (width or height). Images with dimensions larger than the limit will be shrunk while preserving aspect-ratio. By default, there's no limit.", default=0, type=int)
+arg_parser.add_argument("--mp", help="Upper bound for megapixel size of an image. Images larger than the limit will be shrunk while preserving aspect-ratio.", default=64, type=int)
 arg_parser.add_argument("--keep-whitespace", help="If set, no whitespace around images will be removed", default=False, action="store_true")
 args = arg_parser.parse_args()
 
 images_dirname = "images"
 webp_quality = args.q
 remove_whitespace = not args.keep_whitespace
+resize_image = args.r > 0
+resize_image_largest_dimension = args.r
+pixel_count_limit = args.mp * 10 ** 6
 
 # source: https://stackoverflow.com/a/10616717
-def trim(img):
+def trim(img: Image) -> Image:
+    print("Trimming whitespace")
+
     bg = Image.new(img.mode, img.size, img.getpixel((0, 0)))
     diff = ImageChops.difference(img, bg)
     diff = ImageChops.add(diff, diff, 2.0, -100)
@@ -26,6 +34,27 @@ def trim(img):
         return img.crop(bbox)
     else:
         return img
+
+def resize(img: Image) -> None:
+    print("Resizing enabled, checking if any dimension is larger than the limit")
+
+    largest_dimension = max(img.width, img.height)
+    if resize_image_largest_dimension < largest_dimension:
+        print("One of the dimensions is larget than the limit, resizing")
+        shrink_factor = resize_image_largest_dimension / largest_dimension
+        new_size = [floor(img.width * shrink_factor), floor(img.height * shrink_factor)]
+        img.thumbnail(new_size, Image.Resampling.LANCZOS)
+
+def resize_if_over_mp_limit(img: Image) -> None:
+    print("Checking MP limit")
+
+    pixels_total = img.width * img.height
+    if pixels_total > pixel_count_limit:
+        print("Resizing due to MP limit")
+
+        shrink_factor = sqrt(pixel_count_limit / pixels_total)
+        new_size = [floor(img.width * shrink_factor), floor(img.height * shrink_factor)]
+        img.thumbnail(new_size, Image.Resampling.LANCZOS)
 
 def main():
     input_images = listdir(images_dirname)
@@ -45,6 +74,11 @@ def main():
 
         if remove_whitespace:
             image = trim(image)
+
+        if resize_image:
+            resize(image)
+
+        resize_if_over_mp_limit(image)
 
         webp.save_image(image, output_image_path, quality=webp_quality)
 
