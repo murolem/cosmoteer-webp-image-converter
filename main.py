be9795f5887d7bf8ec52583c331b56d8543216aa
aliser <20380250+murolem@users.noreply.github.com>
2025-08-11 18:52:20 +1000

    add parallel processing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import concurrent.futures
 import os
 from math import floor, sqrt
 from os import listdir
@@ -14,6 +15,7 @@ arg_parser.add_argument("-r", help="Upper bound for any image dimension (width o
 arg_parser.add_argument("--mp", help="Upper bound for megapixel size of an image. Images larger than the limit will be shrunk while preserving aspect-ratio.", default=64, type=int)
 arg_parser.add_argument("--keep-whitespace", help="If set, no whitespace around images will be removed", default=False, action="store_true")
 arg_parser.add_argument("--keep-original", help="If set, the original image file will be kept instead of removing/replacing. In an image is already a .webp file, new image will have a suffix (N) appended.", default=False, action="store_true")
+arg_parser.add_argument("--threads", help="Number of parallel threads for processing.", default=5, type=int)
 args = arg_parser.parse_args()
 
 images_dirname = "images"
@@ -24,6 +26,7 @@ resize_image = args.r > 0
 resize_image_largest_dimension = args.r
 pixel_count_limit_mp = args.mp
 pixel_count_limit = pixel_count_limit_mp * 10 ** 6
+threads = args.threads
 
 # source: https://stackoverflow.com/a/10616717
 def trim_whitespace(img: Image) -> Image:
@@ -83,6 +86,48 @@ def generate_unoccupied_filename(dirpath: str, filename: str) -> str:
 
         loop_counter += 1
 
+def process_image(i: int, total: int, image_filename: str) -> None:
+    log_prefix = f"[{i + 1} of {total}]"
+    
+    print_prefixed = lambda text: print(f"{log_prefix} {text}")
+
+    print_prefixed("processing image: " + image_filename)
+
+    input_image_path = os.path.abspath(os.path.join(images_dirname, image_filename))
+
+    output_image_filename = Path(image_filename).stem + ".webp"
+    if keep_original:
+        output_image_filename = generate_unoccupied_filename(images_dirname, output_image_filename)
+    output_image_path = os.path.abspath(os.path.join(images_dirname, output_image_filename))
+
+    image = Image.open(input_image_path)
+
+    try:
+        if remove_whitespace:
+            print_prefixed("Trimming whitespace (if needed)")
+            image = trim_whitespace(image)
+
+        if resize_image:
+            print_prefixed("Resizing based on dimensions (if needed)")
+            resize_based_on_dimensions(image)
+
+        print_prefixed("Resizing based on area (if needed)")
+        resize_based_on_area(image)
+
+        print_prefixed(f"Saving as {output_image_filename}")
+        webp.save_image(image, output_image_path, quality=webp_quality)
+
+        # if keep original is enabled, keep the file.
+        # if source file was a webp image, the new file would have another name (see above)
+        if keep_original:
+            return
+
+        # remove input image after conversion (only if it's not a webp image cuz it would have the same name)
+        if not input_image_path.lower().endswith("webp"):
+            print_prefixed("Removing original")
+            os.remove(input_image_path)
+    finally:
+        image.close()
 
 def main():
     input_images = listdir(images_dirname)
@@ -92,47 +137,12 @@ def main():
         print("no image files found")
         return
 
-    for i, input_image_filename in enumerate(input_images):
-        print(f"[{i + 1} of {len(input_images)}] processing image: " + input_image_filename)
-
-        input_image_path = os.path.abspath(os.path.join(images_dirname, input_image_filename))
-
-        output_image_filename = Path(input_image_filename).stem + ".webp"
-        if keep_original:
-            output_image_filename = generate_unoccupied_filename(images_dirname, output_image_filename)
-        output_image_path = os.path.abspath(os.path.join(images_dirname, output_image_filename))
-
-        image = Image.open(input_image_path)
-
-        try:
-            if remove_whitespace:
-                print("Trimming whitespace (if needed)")
-                image = trim_whitespace(image)
-
-            if resize_image:
-                print("Resizing based on dimensions (if needed)")
-                resize_based_on_dimensions(image)
-
-            print("Resizing based on area (if needed)")
-            resize_based_on_area(image)
-
-            print(f"Saving as {output_image_filename}")
-            webp.save_image(image, output_image_path, quality=webp_quality)
-
-            # if keep original is enabled, keep the file.
-            # if source file was a webp image, the new file would have another name (see above)
-            if keep_original:
-                continue
-
-            # remove input image after conversion (only if it's not a webp image cuz it would have the same name)
-            if not input_image_path.lower().endswith("webp"):
-                print("Removing original")
-                os.remove(input_image_path)
-        finally:
-            image.close()
-
+    total = len(input_images)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=threads) as ex:
+        for i in range (total):
+            ex.submit(process_image, i, total, input_images[i])
+    
     print("all done!")
-
 
 
 if __name__ == '__main__':
